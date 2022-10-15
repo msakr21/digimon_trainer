140c93647fd1b756833745da5126007df3d6f284
Mostafa Sakr <msakr3@gmail.com>
2022-10-15 12:29:24 -0600

    Clean test and html for user story 2.

--- spec/features/trainers/show_spec.rb
+++ spec/features/trainers/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'the trainers show page' do
-  it "displays the trainer name" do
+  it "displays a trainer's information" do
     trainer = Trainer.create!(name: "Mostafa", age: 27, tutorial_completed: false)
     trainer_2 = Trainer.create!(name: "Abbas", age: 28, tutorial_completed: false)
     digimon = trainer.digimons.create!(name: "Agu", kind: "Agumon", starter: true, wins: 9)
@@ -10,10 +10,19 @@ RSpec.describe 'the trainers show page' do
     visit "/trainers/#{trainer.id}"
 
     expect(page).to have_content(trainer.name)
+    expect(page).to have_content(trainer.age)
+    expect(page).to have_content(trainer.tutorial_completed)
     expect(page).to_not have_content(trainer_2.name)
+  end
+
+  it "displays how many digimons a trainer has" do
+    trainer = Trainer.create!(name: "Mostafa", age: 27, tutorial_completed: false)
+    trainer_2 = Trainer.create!(name: "Abbas", age: 28, tutorial_completed: false)
+    digimon = trainer.digimons.create!(name: "Agu", kind: "Agumon", starter: true, wins: 9)
+    digimon_2 = trainer.digimons.create!(name: "Gabu", kind: "Gabumon", starter: false, wins: 7)
+    digimon_3 = trainer_2.digimons.create!(name: "Potato", kind: "Patamon", starter: false, wins: 9)
+    visit "/trainers/#{trainer.id}"
+
     expect(page).to have_content(trainer.digimons_count)
   end
 end
-
-
-#Why does it have to be  for test to work!?
