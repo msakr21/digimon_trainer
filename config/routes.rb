7e27c5ac22baabd00d51e40fce316e790bae7ac2
Mostafa Sakr <msakr3@gmail.com>
2022-10-15 19:21:50 -0600

    Add Create tests and feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/trainers', to: 'trainers#index'
+  get '/trainers/new', to: 'trainers#new'
+  post'/trainers', to: 'trainers#create'
   get '/trainers/:id', to: 'trainers#show'
   get '/digimons', to: 'digimons#index'
   get '/digimons/:id', to: 'digimons#show'
   get '/trainers/:trainer_id/digimons', to: 'trainer_digimons#index'
+  get '/trainers/new', to: 'trainers#new'
 end
